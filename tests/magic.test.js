5c778cf64922023f6d2a7af54964e0da8eb45e38
Guilherme <guilhaugusto05@gmail.com>
2021-10-19 18:24:05 -0300

    Requisito II

--- tests/magic.test.js
+++ tests/magic.test.js
@@ -10,11 +10,18 @@ const expected = {
 };
 
 describe(' Testa a função getMagicCard', () => {
+  const getMagicCardSimulator = trybeSimulator(magic, 'getMagicCard');
   it('Deve retornar um objeto com as propriedades esperadas', async () => {
-    const getMagicCardSimulator = trybeSimulator(magic, 'getMagicCard');
-    await getMagicCardSimulator('130550');
+    expect.assertions(1);
 
-    // implemente seus testes aqui    
-   
+    const magicCard = await getMagicCardSimulator('130550');
+    expect(magicCard).toEqual(expected);
+  });
+  it('A propriedade types é do tipo array', async () => {
+    expect.assertions(1);
+
+    const magicCard = await getMagicCardSimulator('130550');
+    const { types } = magicCard;
+    expect(Array.isArray(types)).toBe(true);
   });
 });
